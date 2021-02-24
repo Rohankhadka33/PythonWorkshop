aa459f112fdb5b5c6240407071ab59e194e36cc8
Dell <rohankhadka33@gmail.com>
2021-02-24 11:49:30 +0545

    name_age

---
+++ pythonlab_1/PythonWorkshop_4.py
@@ -0,0 +1,6 @@
+name= str(input('Enter your name'))
+age= int(input('Enter your age'))
+print('hello my name is ' + name + ' and i am ' + str(age) + ' years old')
+print('hello my name is',name, 'and i am ',age, 'years old')
+print('hello my name is %s and i am %d years old'% (name, age))
+print("hell0 my name is {} and i am {} years old".format(name, age))
