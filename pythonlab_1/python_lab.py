15906bb0e056494b78f2ceb645338bb701d3d32d
Dell <rohankhadka33@gmail.com>
2021-02-24 11:14:05 +0545

    Initial commit

---
+++ pythonlab_1/python_lab.py
@@ -0,0 +1,6 @@
+# Write a program that takes three numvers and prints their sum. Every numbers is given on a seperate  line.
+a= int(input('Enter  the value of a'))
+b= int(input('Enter the value of b'))
+c= int(input('Enter the value of c'))
+sum= a+b+c
+print('The sum of given three number is:',sum)
