a1655c1c019754f6a3c2ad7cbb28a5b09154ad8a
Dell <rohankhadka33@gmail.com>
2021-03-10 11:45:15 +0545

    integer num

--- pythonlab_1/qn no 9.py
+++ pythonlab_1/qn no 9.py
@@ -1,4 +1,7 @@
 '''Write a program to find the sum of the first n positive integers'''
 n= int(input("Enter the number of integers"))
 sum= (n*(n+1))/2
+
+
+
 print(f"The sum of the given first integer is {sum}")
